21548da02f09bcd425c6df0059026ecd46521a1f
Patrick He <77468352+he-patrick@users.noreply.github.com>
2023-12-31 15:16:35 -0500

    Update footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,11 +2,12 @@ import { siteConfig } from "@/../config/site"
 
 export default function Footer() {
   return (
-    <footer className="mt-auto block pb-2 pt-4 text-sm text-zinc-500 dark:text-zinc-400">
-      Contact me: {" "}
-      <a target="_blank" href={"he.patrick2005@gmail.com"} className="underline">
-        Patrick He
+    <footer className="mt-auto block pb-2 pt-4 text-sm text-zinc-500 dark:text-zinc-400 hover:text-zinc-700">
+      I'd love to hear from you! <br />
+      Feel free to drop me an email at: {" "}
+      <a href="mailto:he.patrick2005@gmail.com" className="underline">
+        he.patrick2005@gmail.com
       </a>
     </footer>
-  )
+  );
 }
